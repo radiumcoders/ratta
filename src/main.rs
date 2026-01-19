1293151b881cafc4049c43bb4c2558ae61031b84
Radium Coders <radiumcoders@gmail.com>
2026-01-19 16:44:01 +0530

    feat: enhance TUI app with improved input handling and item management

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ use ratatui::{
     Frame,
     crossterm::event::{ self, Event },
     layout::{ Constraint, Layout },
-    style::{ Color, Stylize },
-    symbols::border,
-    widgets::{ Block, BorderType, List, ListItem, Paragraph, Widget },
+    style::{ Color, Style },
+    widgets::{ Block, BorderType, List, ListItem, ListState, Widget },
 };
 
 #[derive(Debug, Default)]
 struct AppState {
     items: Vec<TodoItem>,
+    list_state: ListState,
 }
 
 #[derive(Debug, Default)]
@@ -24,6 +24,7 @@ fn main() -> Result<()> {
     color_eyre::install()?;
     let mut state = AppState::default();
 
+    // Populate with sample data
     state.items.push(TodoItem {
         title: "Learn Rust".to_string(),
         completed: false,
@@ -32,6 +33,23 @@ fn main() -> Result<()> {
         title: "Build a TUI app".to_string(),
         completed: false,
     });
+    state.items.push(TodoItem {
+        title: "fdfd".to_string(),
+        completed: false,
+    });
+    state.items.push(TodoItem {
+        title: "gege".to_string(),
+        completed: false,
+    });
+    state.items.push(TodoItem {
+        title: "dgdgs".to_string(),
+        completed: false,
+    });
+    state.items.push(TodoItem {
+        title: "sdtst".to_string(),
+        completed: false,
+    });
+    // dummy data end
 
     let terminal = ratatui::init();
     let result = run(terminal, &mut state);
@@ -41,14 +59,29 @@ fn main() -> Result<()> {
 }
 
 fn run(mut terminal: DefaultTerminal, app_state: &mut AppState) -> Result<()> {
-    //renderig
-    terminal.draw(|f| render(f, app_state))?;
-    //handling inputs
     loop {
+        //rendering
+        terminal.draw(|f| render(f, app_state))?;
+        //handling inputs
         if let Event::Key(key) = event::read()? {
+            if key.kind != event::KeyEventKind::Press {
+                continue;
+            }
             match key.code {
                 event::KeyCode::Esc => {
                     break;
+                }
+                event::KeyCode::Char(char) => {
+                    match char {
+                        'j' => app_state.list_state.select_next(),
+                        'k' => app_state.list_state.select_previous(),
+                        'D' => {
+                            if let Some(selected) = app_state.list_state.selected() {
+                                app_state.items.remove(selected);
+                            }
+                        }
+                        _ => {}
+                    }
                 }
                 _ => {}
             }
@@ -57,7 +90,7 @@ fn run(mut terminal: DefaultTerminal, app_state: &mut AppState) -> Result<()> {
     Ok(())
 }
 
-fn render(frame: &mut Frame, app_state: &AppState) {
+fn render(frame: &mut Frame, app_state: &mut AppState) {
     let [border_area] = Layout::vertical([Constraint::Fill(1)])
         .margin(1)
         .areas(frame.area());
@@ -71,8 +104,9 @@ fn render(frame: &mut Frame, app_state: &AppState) {
         // .fg(Color::Yellow)
         .render(border_area, frame.buffer_mut());
 
-    List::new(app_state.items.iter().map(|x| ListItem::from(x.title.clone()))).render(
-        inner_area,
-        frame.buffer_mut()
-    );
+    let list = List::new(app_state.items.iter().map(|x| ListItem::from(x.title.clone())))
+        .highlight_symbol("->")
+        .highlight_style(Style::default().fg(Color::Magenta));
+
+    frame.render_stateful_widget(list, inner_area, &mut app_state.list_state);
 }
